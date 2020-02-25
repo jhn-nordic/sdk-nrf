746bb1a8e310fc676ed78e673b169f322d9d4602
Jon Helge Nistad <jon.helge.nistad@nordicsemi.no>
2020-02-25 23:47:24 +0100

    applications: event_mngr_iot: Initial proof of concept
    Inital work on setting up an event manager based
    architecture for IOT applications.
    
    Signed-off-by: Jon Helge Nistad <jon.helge.nistad@nordicsemi.no>

---
+++ applications/event_mngr_iot/blinky/configuration/led_state.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#ifndef _LED_STATE_H_
+#define _LED_STATE_H_
+
+/**
+ * @brief LED State
+ * @defgroup led_state LED State
+ * @{
+ */
+
+#include <zephyr/types.h>
+#include "led_event.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum led_system_state {
+	LED_SYSTEM_STATE_IDLE,
+	LED_SYSTEM_STATE_CHARGING,
+	LED_SYSTEM_STATE_ERROR,
+
+	LED_SYSTEM_STATE_COUNT
+};
+
+enum led_id {
+	LED_ID_SYSTEM_STATE,
+	LED_ID_COUNT
+};
+
+#define LED_UNAVAILABLE CONFIG_DESKTOP_LED_COUNT
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* _LED_STATE_H_ */
